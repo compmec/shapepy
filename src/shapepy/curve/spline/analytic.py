52bdac2fc90d3b4049b6a657e66a2c416cf5ede5
Carlos Adir <carlos.adir.leite@gmail.com>
2024-11-01 10:54:38 +0100

    feat: add analytic polynomial for splines

---
+++ src/shapepy/curve/spline/analytic.py
@@ -0,0 +1,260 @@
+"""
+This file contains a class Polynomial that allows evaluating and
+making operations with polynomials, like adding, multiplying, etc
+"""
+
+from __future__ import annotations
+
+import math
+from functools import lru_cache
+from typing import Any, Iterable, List, Tuple, Union
+
+from ...core import Parameter, Scalar
+
+
+@lru_cache
+def factorial(n: int) -> int:
+    return math.factorial(int(n))
+
+
+@lru_cache
+def binom(n: int, i: int) -> int:
+    return math.comb(n, i)
+
+
+@lru_cache
+def keys(exp):
+    if exp == 1:
+        return set()
+    return keys(exp // 2) | keys(exp - exp // 2) | {exp}
+
+
+def polymult(
+    poly: Tuple[Scalar, ...], qoly: Tuple[Scalar, ...]
+) -> Tuple[Scalar, ...]:
+    """
+    Multiplies two polynomials
+    """
+    woly: List[Scalar] = [0] * (len(poly) + len(qoly) - 1)
+    for i, pi in enumerate(poly):
+        for j, qj in enumerate(qoly):
+            woly[i + j] += pi * qj
+    return tuple(woly)
+
+
+def polypow(poly: Tuple[Scalar, ...], exp: int) -> Tuple[Scalar, ...]:
+    exp = int(exp)
+    if exp < 1:
+        raise ValueError
+    poly = tuple(poly)
+    needs = sorted(keys(exp))
+    cache = {1: poly}
+    for n in needs:
+        cache[n] = polymult(cache[n // 2], cache[n - n // 2])
+    return cache[exp]
+
+
+def polyderi(poly: Tuple[Scalar, ...], times: int) -> Tuple[Scalar, ...]:
+    times = int(times)
+    if times < 0:
+        raise ValueError
+    if times == 0:
+        return poly
+    if times >= len(poly):
+        return (0 * sum(poly),)
+    return tuple(
+        coef * (factorial(n + times) // factorial(n))
+        for n, coef in enumerate(poly[times:])
+    )
+
+
+class Polynomial:
+    """
+    Defines a polynomial with coefficients
+
+    p(x) = a0 + a1 * x + a2 * x^2 + ... + ap * x^p
+
+    By receiving the coefficients
+
+    coefs = [a0, a1, a2, ..., ap]
+
+    This class allows evaluating, adding, multiplying, etc
+
+    Example
+    -------
+    >>> poly = Polynomial([3, 2])
+    >>> poly(0)
+    3
+    >>> poly(1)
+    5
+    """
+
+    def __init__(self, coefs: Iterable[Any]):
+        self.__coefs = tuple(coefs)
+
+    @property
+    def degree(self) -> int:
+        """
+        Gives the degree of the polynomial
+        """
+        return len(self.__coefs) - 1
+
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, Polynomial):
+            return False
+        if self.degree != other.degree:
+            return False
+        return all(ci == cj for ci, cj in zip(self, other))
+
+    def __iter__(self):
+        yield from self.__coefs
+
+    def __getitem__(self, index):
+        return self.__coefs[index]
+
+    def __neg__(self) -> Polynomial:
+        coefs = tuple(-coef for coef in self)
+        return self.__class__(coefs)
+
+    def __add__(self, other: Union[Any, Polynomial]) -> Polynomial:
+        if isinstance(other, Polynomial):
+            coefs = [0] * (1 + max(self.degree, other.degree))
+            for i, coef in enumerate(self):
+                coefs[i] += coef
+            for i, coef in enumerate(other):
+                coefs[i] += coef
+        else:
+            coefs = list(self)
+            coefs[0] += other
+        return self.__class__(tuple(coefs))
+
+    def __mul__(self, other: Union[Any, Polynomial]) -> Polynomial:
+        if isinstance(other, Polynomial):
+            coefs = polymult(tuple(self), tuple(other))
+        else:
+            coefs = tuple(other * coef for coef in self)
+        return self.__class__(coefs)
+
+    def __truediv__(self, other: Scalar) -> Polynomial:
+        coefs = tuple(coef / other for coef in self)
+        return self.__class__(coefs)
+
+    def __pow__(self, other: int) -> Polynomial:
+        coefs = polypow(self.__coefs, other)
+        return self.__class__(coefs)
+
+    def __sub__(self, other: Union[Any, Polynomial]) -> Polynomial:
+        return self.__add__(-other)
+
+    def __rsub__(self, other: Any) -> Polynomial:
+        return (-self).__add__(other)
+
+    def __radd__(self, other: Any) -> Polynomial:
+        return self.__add__(other)
+
+    def __rmul__(self, other: Any) -> Polynomial:
+        return self.__mul__(other)
+
+    def __call__(self, node: Parameter) -> Any:
+        return self.eval(node, 0)
+
+    def __str__(self):
+        msgs = []
+        if all(coef == 0 for coef in self):
+            return "0"
+        flag = False
+        for i, coef in enumerate(self):
+            if coef == 0:
+                continue
+            if coef < 0:
+                msg = "- "
+            elif flag:
+                msg = "+ "
+            else:
+                msg = ""
+            flag = True
+            coef = abs(coef)
+            if coef != 1 or i == 0:
+                msg += str(coef)
+            if i > 0:
+                if coef != 1:
+                    msg += " * "
+                msg += "x"
+            if i > 1:
+                msg += f"^{i}"
+            msgs.append(msg)
+        return " ".join(msgs)
+
+    def __repr__(self) -> str:
+        return str(self)
+
+    def eval(self, node: Parameter, derivate: int = 0) -> Any:
+        """
+        Evaluates the polynomial at given node
+
+        Example
+        -------
+        >>> poly = Polynomial([1, 2])
+        >>> poly.eval(0)
+        1
+        >>> poly.eval(1)
+        3
+        >>> poly.eval(1, 1)
+        2
+        >>> poly.eval(1, 2)
+        0
+        """
+        coefs = polyderi(self.__coefs, derivate)
+        if len(coefs) == 1:
+            return coefs[0]
+        result = 0 * coefs[0]
+        for coef in coefs[::-1]:
+            result = node * result + coef
+        return result
+
+    def derivate(self, times: int = 1) -> Polynomial:
+        """
+        Derivate the polynomial curve, giving a new one
+
+        Example
+        -------
+        >>> poly = Polynomial([1, 2, 5])
+        >>> print(poly)
+        1 + 2 * x + 5 * x^2
+        >>> dpoly = poly.derivate()
+        >>> print(dpoly)
+        2 + 10 * x
+        """
+        coefs = polyderi(self.__coefs, times)
+        return self.__class__(coefs)
+
+    def shift(self, amount: Parameter) -> Polynomial:
+        """
+        Transforms the polynomial p(x) into p(x-d) by
+        translating the curve by 'd' to the right.
+
+        p(x) = a0 + a1 * x + ... + ap * x^p
+        p(x-d) = a0 + a1 * (x-d) + ... + ap * (x-d)^p
+               = b0 + b1 * x + ... + bp * x^p
+
+        Example
+        -------
+        >>> old_poly = Polynomial([0, 0, 0, 1])
+        >>> print(old_poly)
+        x^3
+        >>> new_poly = poly.shift(1)  # transform to (x-1)^3
+        >>> print(new_poly)
+        - 1 + 3 * x - 3 * x^2 + x^3
+        """
+        newcoefs = list(self)
+        for i, coef in enumerate(self):
+            for j in range(i):
+                value = binom(i, j) * (amount ** (i - j))
+                if (i + j) % 2:
+                    value *= -1
+                newcoefs[j] += coef * value
+        return self.__class__(tuple(newcoefs))
+
+    def scale(self, amount: Scalar) -> Polynomial:
+        coefs = tuple(coef * amount**i for i, coef in enumerate(self))
+        return self.__class__(coefs)
---
+++ tests/curve/spline/test_analytic.py
@@ -0,0 +1,308 @@
+"""
+This file contains tests functions to test the module polygon.py
+"""
+
+import pytest
+
+from shapepy.curve.spline.analytic import Polynomial
+
+
+@pytest.mark.order(3)
+@pytest.mark.dependency()
+def test_begin():
+    pass
+
+
+@pytest.mark.order(3)
+@pytest.mark.dependency(depends=["test_begin"])
+def test_build():
+    Polynomial([1])  # p(x) = 1
+    Polynomial([1, 1])  # p(x) = 1 + x
+    Polynomial([1, 2])  # p(x) = 1 + 2*x
+    Polynomial([1, 3, 2])  # 1 + 3*x + 2*x^2
+    Polynomial([1.0, 2.0, 3.0])
+
+
+@pytest.mark.order(3)
+@pytest.mark.dependency(depends=["test_begin"])
+def test_degree():
+    assert Polynomial([1]).degree == 0
+    assert Polynomial([1, 1]).degree == 1
+    assert Polynomial([1, 2]).degree == 1
+    assert Polynomial([1, 3, 2]).degree == 2
+    assert Polynomial([1.0, 2.0, 3.0]).degree == 2
+
+
+@pytest.mark.order(3)
+@pytest.mark.dependency(depends=["test_build"])
+def test_evaluate_natural():
+    poly = Polynomial([1])  # p(x) = 1
+    assert poly.eval(0) == 1
+    assert poly.eval(1) == 1
+    assert poly.eval(2) == 1
+    assert poly.eval(1.0) == 1
+
+    poly = Polynomial([1, 2])  # p(x) = 1 + 2*x
+    assert poly.eval(0) == 1
+    assert poly.eval(1) == 3
+    assert poly.eval(2) == 5
+    assert poly.eval(0.0) == 1
+    assert poly.eval(1.0) == 3
+
+    poly = Polynomial([1, 2, 3])  # p(x) = 1 + 2*x + 3*x^2
+    assert poly.eval(0) == 1
+    assert poly.eval(1) == 6
+    assert poly.eval(2) == 17
+    assert poly.eval(0.0) == 1
+    assert poly.eval(1.0) == 6
+
+
+@pytest.mark.order(3)
+@pytest.mark.dependency(
+    depends=["test_evaluate_natural", "test_evaluate_derivate", "test_degree"]
+)
+def test_keep_type():
+    coefs = [3, 5, -4, 3, 2]
+    poly = Polynomial(coefs)
+    for deriv in range(0, poly.degree):
+        for node in range(-5, 6):
+            val = poly.eval(node, deriv)
+            assert isinstance(val, int)
+        for node in range(-5, 6):
+            val = poly.eval(float(node), deriv)
+            assert isinstance(val, float)
+
+    coefs = [3.0, 5.0, -4.0, 3.0, 2.0]
+    poly = Polynomial(coefs)
+    for deriv in range(0, poly.degree):
+        for node in range(-5, 6):
+            val = poly.eval(node, deriv)
+            assert isinstance(val, float)
+        for node in range(-5, 6):
+            val = poly.eval(float(node), deriv)
+            assert isinstance(val, float)
+
+    from fractions import Fraction
+
+    one = Fraction(1)
+    coefs = [3 * one, 5 * one, -4 * one, 3 * one, 2 * one]
+    poly = Polynomial(coefs)
+    for deriv in range(0, poly.degree):
+        for node in range(-5, 6):
+            val = poly.eval(node, deriv)
+            assert isinstance(val, Fraction)
+        for node in range(-5, 6):
+            val = poly.eval(float(node), deriv)
+            assert isinstance(val, float)
+
+
+@pytest.mark.order(3)
+@pytest.mark.dependency(depends=["test_degree"])
+def test_compare():
+    coefs = [3, 5, -4, 3, 2]
+    polya = Polynomial(coefs)
+    coefs = [3, 5, -4, 3, 2, 7]
+    polyb = Polynomial(coefs)
+    assert polya != polyb
+
+    coefs = [3, 5, -4, 3, 2]
+    polya = Polynomial(coefs)
+    coefs = [3, 5, -4, 3, 2]
+    polyb = Polynomial(coefs)
+    assert polya == polyb
+
+    coefs = [3, 5, -4, 3, 2]
+    polya = Polynomial(coefs)
+    coefs = [3, -3, -4, 3, 2]
+    polyb = Polynomial(coefs)
+    assert polya != polyb
+
+
+@pytest.mark.order(3)
+@pytest.mark.dependency(depends=["test_compare"])
+def test_derivate():
+    coefs = [3, 5, -4, 3, 2]
+    poly = Polynomial(coefs)
+    test = poly.derivate(0)
+    assert test == poly
+
+    test = poly.derivate(1)
+    good = Polynomial([5, -8, 9, 8])
+    assert test != poly
+    assert test == good
+
+    test = poly.derivate(2)
+    good = Polynomial([-8, 18, 24])
+    assert test != poly
+    assert test == good
+
+
+@pytest.mark.order(3)
+@pytest.mark.dependency(depends=["test_evaluate_natural", "test_derivate"])
+def test_evaluate_derivate():
+    poly = Polynomial([1])  # p(x) = 1
+    assert poly.eval(0, 1) == 0
+    assert poly.eval(1, 1) == 0
+    assert poly.eval(2, 1) == 0
+    assert poly.eval(1.0, 1) == 0
+    assert poly.eval(1, 2) == 0
+
+    poly = Polynomial([1, 2])  # p(x) = 1 + 2*x
+    assert poly.eval(0, 1) == 2
+    assert poly.eval(1, 1) == 2
+    assert poly.eval(2, 1) == 2
+    assert poly.eval(0.0, 1) == 2
+    assert poly.eval(1.0, 1) == 2
+    assert poly.eval(2, 2) == 0
+    assert poly.eval(0.0, 2) == 0
+
+    poly = Polynomial([1, 2, 3])  # p(x) = 1 + 2*x + 3*x^2
+    assert poly.eval(0, 1) == 2
+    assert poly.eval(1, 1) == 8
+    assert poly.eval(2, 1) == 14
+    assert poly.eval(0.0, 1) == 2
+    assert poly.eval(1.0, 1) == 8
+    assert poly.eval(2, 2) == 6
+    assert poly.eval(0.0, 2) == 6
+    assert poly.eval(2, 3) == 0
+    assert poly.eval(0.0, 3) == 0
+
+
+@pytest.mark.order(3)
+@pytest.mark.dependency(depends=["test_compare"])
+def test_add():
+    polya = Polynomial([1, 3])
+    assert polya + 3 == Polynomial([4, 3])
+    assert 3 + polya == Polynomial([4, 3])
+
+    polya = Polynomial([1, 2, 3])
+    polyb = Polynomial([3, 2, 1])
+    assert polya + polyb == Polynomial([4, 4, 4])
+
+    polya = Polynomial([1, 2, 3])
+    polyb = Polynomial([3, 2, 1, 7])
+    assert polya + polyb == Polynomial([4, 4, 4, 7])
+
+    polya = Polynomial([1, 2, 3, 7])
+    polyb = Polynomial([3, 2, 1])
+    assert polya + polyb == Polynomial([4, 4, 4, 7])
+
+
+@pytest.mark.order(3)
+@pytest.mark.dependency(depends=["test_compare"])
+def test_sub():
+    polya = Polynomial([1, 3])
+    assert polya - 3 == Polynomial([-2, 3])
+    assert 3 - polya == Polynomial([2, -3])
+
+
+@pytest.mark.order(3)
+@pytest.mark.dependency(depends=["test_compare"])
+def test_mult():
+    polya = Polynomial([1, 3])
+    assert 4 * polya == Polynomial([4, 12])
+    assert polya * 4 == Polynomial([4, 12])
+
+    polya = Polynomial([1, 1])
+    polyb = Polynomial([1, 1])
+    assert polya * polyb == Polynomial([1, 2, 1])
+
+    polya = Polynomial([1, 1])
+    polyb = Polynomial([1, 2, 1])
+    assert polya * polyb == Polynomial([1, 3, 3, 1])
+
+    polya = Polynomial([1, 2, 1])
+    polyb = Polynomial([1, 2, 1])
+    assert polya * polyb == Polynomial([1, 4, 6, 4, 1])
+
+    polya = Polynomial([1, 1])
+    polyb = Polynomial([1, 3, 3, 1])
+    assert polya * polyb == Polynomial([1, 4, 6, 4, 1])
+
+
+@pytest.mark.order(3)
+@pytest.mark.dependency(depends=["test_compare"])
+def test_pow():
+    poly = Polynomial([1, 1])
+    assert poly**1 == Polynomial([1, 1])
+    assert poly**2 == Polynomial([1, 2, 1])
+    assert poly**3 == Polynomial([1, 3, 3, 1])
+    assert poly**4 == Polynomial([1, 4, 6, 4, 1])
+
+    poly = Polynomial([1, 2])
+    assert poly**1 == Polynomial([1, 2])
+    assert poly**2 == Polynomial([1, 4, 4])
+    assert poly**3 == Polynomial([1, 6, 12, 8])
+    assert poly**4 == Polynomial([1, 8, 24, 32, 16])
+
+
+@pytest.mark.order(3)
+@pytest.mark.dependency(depends=["test_compare"])
+def test_shift():
+    poly = Polynomial([-1, 3, -3, 1])  # (x-1)^3
+    assert poly.shift(-1) == Polynomial([0, 0, 0, 1])  # x^3
+
+    poly = Polynomial([1, 8, 24, 32, 16])  # (1+2*x)^4
+    assert poly.shift(1 / 2) == Polynomial([0, 0, 0, 0, 16])  # (2*x)^4
+
+
+@pytest.mark.order(3)
+@pytest.mark.dependency(depends=["test_compare"])
+def test_scale():
+    poly = Polynomial([1, 2, 3])  # 1 + 2*x + 3*x^2
+    assert poly.scale(2) == Polynomial([1, 4, 12])
+
+
+@pytest.mark.order(3)
+@pytest.mark.dependency(depends=["test_build"])
+def test_print():
+    poly = Polynomial([0])
+    assert str(poly) == "0"
+    poly = Polynomial([0, 0])
+    assert str(poly) == "0"
+    poly = Polynomial([0, 0, 0])
+    assert str(poly) == "0"
+    poly = Polynomial([1])
+    assert str(poly) == "1"
+    poly = Polynomial([1, 1])
+    assert str(poly) == "1 + x"
+    poly = Polynomial([1, 1, 1])
+    assert str(poly) == "1 + x + x^2"
+    poly = Polynomial([1, 0, 1])
+    assert str(poly) == "1 + x^2"
+    poly = Polynomial([0, 0, 1])
+    assert str(poly) == "x^2"
+    poly = Polynomial([-1])
+    assert str(poly) == "- 1"
+    poly = Polynomial([-1, 1])
+    assert str(poly) == "- 1 + x"
+    poly = Polynomial([1, -1, 1])
+    assert str(poly) == "1 - x + x^2"
+    poly = Polynomial([1, 0, 1])
+    assert str(poly) == "1 + x^2"
+    poly = Polynomial([1, 2, 3])
+    assert str(poly) == "1 + 2 * x + 3 * x^2"
+    repr(poly)
+
+
+@pytest.mark.order(3)
+@pytest.mark.dependency(
+    depends=[
+        "test_begin",
+        "test_build",
+        "test_degree",
+        "test_evaluate_natural",
+        "test_keep_type",
+        "test_compare",
+        "test_derivate",
+        "test_evaluate_derivate",
+        "test_add",
+        "test_sub",
+        "test_mult",
+        "test_pow",
+        "test_shift",
+        "test_scale",
+    ]
+)
+def test_end():
+    pass
