929d614a87c16744418626d00b8dd3802fa395c1
Carlos Adir <carlos.adir.leite@gmail.com>
2024-11-26 21:07:26 +0100

    feat: add intersection and concatenate of curves

---
+++ src/shapepy/curve/concatenate.py
@@ -0,0 +1,49 @@
+from ..core import ICurve
+from .abc import IClosedCurve, IJordanCurve, IOpenCurve, IParameterCurve
+from .polygon import JordanPolygon, PolygonClosedCurve, PolygonOpenCurve
+
+
+def concatenate(*curves: IOpenCurve) -> IParameterCurve:
+    curves = tuple(curves)
+    for curve in curves:
+        if not isinstance(curve, IOpenCurve):
+            raise TypeError
+    if all(isinstance(curve, PolygonOpenCurve) for curve in curves):
+        return concatenate_polygon(*curves)
+    raise NotImplementedError("Not expected get here")
+
+
+def concatenate_polygon(*curves: PolygonOpenCurve) -> IParameterCurve:
+    curves = tuple(curves)
+    for curve in curves:
+        if not isinstance(curve, PolygonOpenCurve):
+            raise TypeError
+    final = curves[0]
+    for curve in curves[1:]:
+        if final.vertices[-1] != curve.vertices[0]:
+            raise ValueError
+        vertices = list(final.vertices) + list(curve.vertices[1:])
+        final = PolygonOpenCurve(vertices)
+    return final
+
+
+def closes_curve(curve: IOpenCurve) -> IClosedCurve:
+    if not isinstance(curve, IOpenCurve):
+        raise TypeError
+    if curve.vertices[0] != curve.vertices[-1]:
+        raise ValueError
+    if isinstance(curve, PolygonOpenCurve):
+        return PolygonClosedCurve(curve.vertices[:-1])
+    raise NotImplementedError("Not expected get here")
+
+
+def transform_to_jordan(curve: ICurve) -> IJordanCurve:
+    if not isinstance(curve, ICurve):
+        raise TypeError
+    if isinstance(curve, IJordanCurve):
+        return curve
+    if isinstance(curve, IOpenCurve):
+        curve = closes_curve(curve)
+    if isinstance(curve, PolygonClosedCurve):
+        return JordanPolygon(curve.vertices)
+    raise NotImplementedError("Not expected get here")
---
+++ src/shapepy/curve/intersect.py
@@ -0,0 +1,84 @@
+from ..boolean import BoolOr
+from ..core import Empty, IBoolean2D, ICurve
+from .abc import IClosedCurve, IJordanCurve
+from .polygon import PolygonClosedCurve
+
+
+def curve_and_curve(curvea: ICurve, curveb: ICurve) -> IBoolean2D:
+    if not isinstance(curvea, ICurve):
+        raise TypeError
+    if not isinstance(curveb, ICurve):
+        raise TypeError
+    if isinstance(curvea, IJordanCurve) and isinstance(curveb, IJordanCurve):
+        return jordan_and_jordan(curvea, curveb)
+    if isinstance(curvea, IClosedCurve) and isinstance(curveb, IClosedCurve):
+        return closed_and_closed(curvea, curveb)
+    raise NotImplementedError(
+        f"Not expected: {type(curvea)} and {type(curveb)}"
+    )
+
+
+def jordan_and_jordan(
+    curvea: IJordanCurve, curveb: IJordanCurve
+) -> IBoolean2D:
+    if not isinstance(curvea, IJordanCurve):
+        raise TypeError
+    if not isinstance(curveb, IJordanCurve):
+        raise TypeError
+    return curve_and_curve(curvea.param_curve, curveb.param_curve)
+
+
+def closed_and_closed(
+    curvea: IClosedCurve, curveb: IClosedCurve
+) -> IBoolean2D:
+    if not isinstance(curvea, IClosedCurve):
+        raise TypeError
+    if not isinstance(curveb, IClosedCurve):
+        raise TypeError
+    if isinstance(curvea, PolygonClosedCurve) and isinstance(
+        curveb, PolygonClosedCurve
+    ):
+        return polygon_and_polygon(curvea, curveb)
+    raise NotImplementedError(
+        f"Not expected: {type(curvea)} and {type(curveb)}"
+    )
+
+
+def polygon_and_polygon(
+    curvea: PolygonClosedCurve, curveb: PolygonClosedCurve
+) -> IBoolean2D:
+    if not isinstance(curvea, PolygonClosedCurve):
+        raise TypeError
+    if not isinstance(curveb, PolygonClosedCurve):
+        raise TypeError
+    inters = []
+    for i, avect in enumerate(curvea.vectors):
+        avertex = curvea.vertices[i]
+        for j, bvect in enumerate(curveb.vectors):
+            bvertex = curveb.vertices[j]
+            denom = bvect.cross(avect)
+            diforigin = bvertex - avertex
+            if denom != 0:  # Not Parallel lines
+                tparam = bvect.cross(diforigin) / denom
+                if tparam < 0 or 1 < tparam:
+                    continue
+                uparam = avect.cross(diforigin) / denom
+                if uparam < 0 or 1 < uparam:
+                    continue
+                point = curvea.eval(i + tparam)
+                inters.append(point)
+                continue
+            if avect.cross(diforigin):
+                continue  # Not coincident lines
+            paramc = avect.inner(diforigin) / avect.inner(avect)
+            if paramc < 0 or 1 < paramc:
+                continue
+            paramd = avect.inner(diforigin + bvect) / avect.inner(avect)
+            if paramd < 0 or 1 < paramd:
+                continue
+            raise NotImplementedError
+    if not len(inters):
+        return Empty()
+    if len(inters) == 1:
+        return inters[0]
+    return BoolOr(inters)
