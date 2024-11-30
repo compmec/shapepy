32fc2029ae82f815a07ac977961752fe237da314
Carlos Adir <carlos.adir.leite@gmail.com>
2024-11-30 21:35:06 +0100

    refactor: remove methods like `move`, `rotate` from JordanSpline

--- src/shapepy/curve/spline/jordan.py
+++ src/shapepy/curve/spline/jordan.py
@@ -7,8 +7,6 @@ from __future__ import annotations
 
 from typing import Tuple
 
-import numpy as np
-
 from ...core import Scalar
 from ...point import GeneralPoint, Point2D
 from ..abc import IJordanCurve
@@ -54,95 +52,6 @@ class JordanSpline(IJordanCurve):
     def vertices(self) -> Tuple[Point2D, ...]:
         raise NotImplementedError
 
-    def move(self, vector: GeneralPoint) -> JordanSpline:
-        """Translate the entire curve by ``point``
-
-        :param point: The translation amount
-        :type point: Point2D
-        :return: The same curve
-        :rtype: JordanCurve
-
-        Example use
-        -----------
-
-        >>> from shapepy import JordanCurve
-        >>> vertices = [(0, 0), (4, 0), (0, 3)]
-        >>> jordan = JordanPolygon(vertices)
-        >>> jordan.move((2, 3))
-        Jordan Curve of degree 1 and vertices
-        ((2, 3), (6, 3), (2, 6))
-
-        """
-        if not isinstance(vector, Point2D):
-            vector = Point2D(vector)
-        ctrlpoints = tuple(point.move(vector) for point in self.ctrlpoints)
-        self.__param_curve = SplineClosedCurve(self.knotvector, ctrlpoints)
-        return self
-
-    def scale(self, xscale: float, yscale: float) -> JordanSpline:
-        """Scale the entire curve in horizontal and vertical direction
-
-        :param xscale: The scale in horizontal direction
-        :type xscale: float
-        :param yscale: The scale in vertical direction
-        :type yscale: float
-        :return: The same curve
-        :rtype: JordanCurve
-
-        Example use
-        -----------
-
-        >>> from shapepy import JordanCurve
-        >>> vertices = [(0, 0), (4, 0), (0, 3)]
-        >>> jordan = JordanPolygon(vertices)
-        >>> jordan.scale(2, 3)
-        Jordan Curve of degree 1 and vertices
-        ((0, 0), (8, 0), (0, 9))
-        >>> jordan.scale(1/2, 1/3)
-        Jordan Curve of degree 1 and vertices
-        ((0.0, 0.0), (4.0, 0.0), (0.0, 3.0))
-
-        """
-        float(xscale)
-        float(yscale)
-        ctrlpoints = tuple(
-            point.scale(xscale, yscale) for point in self.ctrlpoints
-        )
-        self.__param_curve = SplineClosedCurve(self.knotvector, ctrlpoints)
-        return self
-
-    def rotate(self, angle: float, degrees: bool = False) -> JordanSpline:
-        """Rotate the entire curve around the origin
-
-        :param angle: The amount to rotate
-        :type angle: float
-        :param degrees: If the angle is in radians (``degrees=False``)
-        :type degrees: bool(, optional)
-        :return: The same curve
-        :rtype: JordanCurve
-
-        Example use
-        -----------
-
-        >>> import math
-        >>> from shapepy import JordanCurve
-        >>> vertices = [(0, 0), (4, 0), (0, 3)]
-        >>> jordan = JordanPolygon(vertices)
-        >>> jordan.rotate(math.pi)
-        Jordan Curve of degree 1 and vertices
-        ((-0.0, 0.0), (-4.0, 4.899e-16), (-3.674e-16, -3.0))
-        >>> jordan.rotate(180, degrees=True)
-        Jordan Curve of degree 1 and vertices
-        ((0.0, -0.0), (4.0, -9.797e-16), (7.348e-16, 3.0))
-
-        """
-        float(angle)
-        if degrees:
-            angle *= np.pi / 180
-        ctrlpoints = tuple(point.rotate(angle) for point in self.ctrlpoints)
-        self.__param_curve = SplineClosedCurve(self.knotvector, ctrlpoints)
-        return self
-
     def __str__(self) -> str:
         degree, npts = self.knotvector.degree, self.knotvector.npts
         msg = f"Jordan Spline of degree {degree} and {npts} points\n"
@@ -152,3 +61,6 @@ class JordanSpline(IJordanCurve):
     def __repr__(self) -> str:
         degree, npts = self.knotvector.degree, self.knotvector.npts
         return f"JordanCurve (deg {degree}, {npts})"
+
+    def winding(self, point: GeneralPoint) -> Scalar:
+        return self.param_curve.winding(point)
