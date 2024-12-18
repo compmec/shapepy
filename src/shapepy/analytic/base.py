078f3033c2065867f0af9f58fa0bd924d5ffb996
carlos-adir <carlos.adir.leite@gmail.com>
2024-12-18 14:20:37 -0300

    style: satisfy pylint requirements for base analytic class

--- src/shapepy/analytic/base.py
+++ src/shapepy/analytic/base.py
@@ -1,5 +1,12 @@
+"""
+This file contains the class BaseAnalytic that serves as base class for other
+analytic functions like Polynomial, Trignomial and Hypernomial
+
+It states the natural relations of iteration, __sub__, __pow__, etc
+"""
 from __future__ import annotations
 
+from abc import abstractmethod
 from functools import lru_cache
 from typing import Any, Iterable, Union
 
@@ -8,12 +15,27 @@ from ..core import IAnalytic, Parameter, Scalar
 
 @lru_cache
 def keys_pow(exp):
+    """
+    Computes the degrees of pow that needs to use in __pow__
+
+    keys(1)  # {}
+    keys(2)  # {2}
+    keys(3)  # {2, 3}
+    keys(4)  # {2, 4}
+    keys(12)  # {2, 3, 6, 12}
+    keys(18)  # {2, 3, 4, 5, 9, 18}
+    """
     if exp == 1:
         return set()
     return keys_pow(exp // 2) | keys_pow(exp - exp // 2) | {exp}
 
 
 class BaseAnalytic(IAnalytic):
+    """
+    BaseAnalytic class serves as base class for other analytic classes
+    like Polynomial, Trignomial and Hypernomial.
+    """
+
     def __init__(self, coefs: Iterable[Scalar]):
         self.__coefs = list(coefs)
 
@@ -52,6 +74,22 @@ class BaseAnalytic(IAnalytic):
         for need in needs:
             cache[need] = cache[need // 2] * cache[need - need // 2]
         return cache[exponent]
+
+    @abstractmethod
+    def __add__(self, other: Any) -> BaseAnalytic:
+        raise NotImplementedError
+
+    @abstractmethod
+    def __neg__(self) -> BaseAnalytic:
+        raise NotImplementedError
+
+    @abstractmethod
+    def __mul__(self, other: Any) -> BaseAnalytic:
+        raise NotImplementedError
+
+    @abstractmethod
+    def __divmod__(self, other: Any) -> BaseAnalytic:
+        raise NotImplementedError
 
     def __sub__(self, other: Union[Any, BaseAnalytic]) -> BaseAnalytic:
         return self.__add__(-other)
