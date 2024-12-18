12961cf5cc463290fdc93fa3efe5b66e0e8a862d
carlos-adir <carlos.adir.leite@gmail.com>
2024-12-18 15:44:23 -0300

    docs: add docstrings for utility functions

--- src/shapepy/utils.py
+++ src/shapepy/utils.py
@@ -1,7 +1,27 @@
+"""
+Utility functions for the package
+"""
 from typing import Any, Iterable, Tuple
 
 
 def sorter(items: Iterable[Any], /, *, reverse: bool = False) -> Iterable[int]:
+    """
+    Gives the indexs of sorting of the objects
+
+    Parameters
+    ---------
+    items: iterable[Any]
+        The itens that must be sorted
+    reverse: bool, default = False
+        To switch between increasing/decreasing order
+
+    Example
+    -------
+    >>> sorter([3, 4, 5])
+    (0, 1, 2)
+    >>> sorter([3, 5, 4])
+    (0, 2, 1)
+    """
     items = tuple(items)
     values = sorted(zip(items, range(len(items))), reverse=reverse)
     return (vs[-1] for vs in values)
@@ -9,12 +29,16 @@ def sorter(items: Iterable[Any], /, *, reverse: bool = False) -> Iterable[int]:
 
 def permutations(*numbers: int) -> Iterable[Tuple[int, ...]]:
     """
-    Permutes the numbers
+    Permutes the numbers using tensorproduct
 
-    Example:
-    permutations(4) -> [0, 1, 2, 3]
-    permutations(4, 2) -> [(0, 0), (0, 1), (1, 0), ..., (3, 0), (3, 0)]
-    permutations(2, 4, 1) -> [(0, 0, 0), ..., (1, 3, 1), (1, 3, 2)]
+    Example
+    -------
+    >>> permutations(4)
+    [0, 1, 2, 3]
+    >>> permutations(4, 2)
+    [(0, 0), (0, 1), (1, 0), ..., (3, 0), (3, 0)]
+    >>> permutations(2, 4, 1)
+    [(0, 0, 0), ..., (1, 3, 1), (1, 3, 2)]
     """
     if len(numbers) == 1:
         for j in range(numbers[0]):
