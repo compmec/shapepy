a4334ecee06c737d06aa4a9243e1fc0d518c7878
carlos-adir <carlos.adir.leite@gmail.com>
2024-12-18 10:04:46 -0300

    feat: add hyperbolic analytic function

---
+++ src/shapepy/analytic/hyperbolic.py
@@ -0,0 +1,355 @@
+"""
+This file contains a class Hypernomial that allows evaluating and
+making operations with hypernomials, like adding, multiplying, etc
+
+A hypernomial is similar to a trignomials, but uses hyperbolic functions :
+    * sinh(t) instead of sin(t)
+    * cosh(t) instead of cos(t)
+
+Example
+-------
+>>> hyper = Hypernomial([1, 2, 3])
+>>> print(hyper)
+1 + 2 * sinh(x) + 3 * cosh(x)
+>>> hyper = Hypernomial([1, 2, 3, 4])
+>>> print(hyper)
+1 + 2 * sinh(x) + 3 * cosh(x) + 4 * sinh(2*x)
+"""
+
+from __future__ import annotations
+
+import math
+from fractions import Fraction
+from functools import lru_cache
+from typing import Iterable, Optional, Tuple, Union
+
+from ..core import IAnalytic
+
+Parameter = Union[int, float]
+Scalar = Union[int, float]
+
+
+@lru_cache
+def keys(exp):
+    if exp == 1:
+        return set()
+    return keys(exp // 2) | keys(exp - exp // 2) | {exp}
+
+
+class Hypernomial(IAnalytic):
+    """
+    Defines a hyberbolic version of a trignomials
+
+    We define a hyperbolic version:
+    H(x) = a0 + a1 * sinh(w*x) + a2 * cosh(w*x) + ...
+           + a_{2p-1} * sinh(p*w*x) + a_{2p} * cosh(p*w*x)
+
+    Example
+    -------
+    >>> hyper = Hypernomial([1, 0, 1])
+    >>> print(hyper)
+    1 + cosh(x)
+    >>> hyper.eval(0)
+    2.0
+    >>> hyper.eval(1)
+    2.54308063482
+    >>> hyper = Hypernomial([1, 2, 0, -3])
+    >>> print(hyper)
+    1 + 2 * sinh(x) - 3*sinh(2*x)
+
+    """
+
+    def __init__(self, values: Iterable[Scalar], frequency: Scalar = 1):
+        values = tuple(values)
+        if not values:
+            raise ValueError("Cannot pass empty tuple")
+        degree = max((i for i, ci in enumerate(values) if ci), default=0)
+        values = list(values[: degree + 1])
+        if not len(values) % 2:
+            values.append(0 * sum(values))
+        for i, value in enumerate(values):
+            if isinstance(value, int):
+                values[i] = Fraction(value)
+        self.__values = values
+        self.__freq = frequency
+
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, IAnalytic):
+            return self.pmax == 0 and self[0] == other
+        if not isinstance(other, Hypernomial):
+            return False
+        if self.pmax != other.pmax or self.frequency != other.frequency:
+            return False
+        atol = 1e-9
+        return all(abs(ci - cj) < atol for ci, cj in zip(self, other))
+
+    @property
+    def pmax(self) -> int:
+        """
+        Returns the maximum harmonic value of the hypernomio
+
+        Example
+        -------
+        >>> hyper = Hypernomial([1, 0, 1])  # 1 + cosh(x)
+        >>> hyper.pmax
+        1
+        >>> hyper = Hypernomial([1, 0, 1, 2])  # 1 + cosh(x) + sinh(2*x)
+        >>> hyper.pmax
+        2
+        """
+        return len(self.__values) // 2
+
+    @property
+    def frequency(self) -> Scalar:
+        """
+        Returns the frequency of the hypernomio
+
+        Example
+        -------
+        >>> hyper = Hypernomial([1, 0, 1])  # 1 + cosh(x)
+        >>> hyper.frequency
+        1
+        >>> hyper = Hypernomial([1, 0, 1], 3)  # 1 + cosh(3*x)
+        >>> hyper.frequency
+        3
+        """
+        return self.__freq
+
+    def __iter__(self):
+        yield from self.__values
+
+    def __getitem__(self, index):
+        return self.__values[index]
+
+    def __setitem__(self, key, newvalue):
+        self.__values[key] = newvalue
+
+    def __neg__(self) -> Hypernomial:
+        return self.__class__(tuple(-coef for coef in self), self.frequency)
+
+    def __add__(self, other: Union[Scalar, Hypernomial]):
+        if not isinstance(other, Hypernomial):
+            coefs = list(self)
+            coefs[0] += other
+            return self.__class__(coefs, self.frequency)
+        if self.frequency != other.frequency:
+            raise NotImplementedError
+        values = [0] * (1 + 2 * max(self.pmax, other.pmax))
+        for i, val in enumerate(self):
+            values[i] += val
+        for i, val in enumerate(other):
+            values[i] += val
+        return self.__class__(values, self.frequency)
+
+    def __sub__(self, other: Union[Scalar, Hypernomial]) -> Hypernomial:
+        return self.__add__(-other)
+
+    def __mul__(self, other: Union[Scalar, Hypernomial]) -> Hypernomial:
+        if not isinstance(other, Hypernomial):
+            coefs = tuple(other * coef for coef in self)
+            return self.__class__(coefs, self.frequency)
+        if self.frequency != other.frequency:
+            raise NotImplementedError
+        values = [0] * (1 + 2 * (self.pmax + other.pmax))
+        for i, vali in enumerate(self):
+            p = (i + 1) // 2
+            for j, valj in enumerate(other):
+                q = (j + 1) // 2
+                s, d = p + q, abs(p - q)
+                temp = (vali * valj) / 2
+                if (i % 2) ^ (j % 2):
+                    if p != q:
+                        sind = 1 if (i > j) ^ (i % 2) else 1
+                        values[2 * d - 1] += temp * sind
+                    values[2 * s - 1] += temp
+                else:
+                    coss = -1 if i % 2 else 1
+                    values[2 * d] += temp * coss
+                    values[2 * s] += temp
+        return self.__class__(values, self.frequency)
+
+    def __divmod__(
+        self, other: Union[Scalar, Hypernomial]
+    ) -> Tuple[Hypernomial, Hypernomial]:
+        if isinstance(other, Hypernomial) and not other.pmax:
+            return self.__divmod__(other[0])
+        if not isinstance(other, Hypernomial):
+            if not other:
+                raise ZeroDivisionError("division by zero")
+            quot = self.__class__((ci / other for ci in self), self.frequency)
+            rest = Hypernomial([0 * sum(self)])
+            return quot, rest
+        raise NotImplementedError
+
+    def __floordiv__(self, other: Union[Scalar, Hypernomial]) -> Hypernomial:
+        return self.__divmod__(other)[0]
+
+    def __mod__(self, other: Union[Scalar, Hypernomial]) -> Hypernomial:
+        return self.__divmod__(other)[1]
+
+    def __truediv__(self, other: Scalar) -> Hypernomial:
+        div, res = self.__divmod__(other)
+        if res != 0:
+            raise ValueError(f"Cannot divide {self} by {other}")
+        return div
+
+    def __rsub__(self, other: Scalar) -> Hypernomial:
+        return (-self).__add__(other)
+
+    def __rmul__(self, other: Scalar) -> Hypernomial:
+        return self.__mul__(other)
+
+    def __radd__(self, other: Scalar) -> Hypernomial:
+        return self.__add__(other)
+
+    def __pow__(self, exponent: int) -> Hypernomial:
+        exponent = int(exponent)
+        if exponent < 0:
+            raise ValueError
+        if exponent == 0:
+            return self.__class__([1 + 0 * sum(self)])
+        needs = sorted(keys(exponent))
+        cache = {1: self}
+        for n in needs:
+            cache[n] = cache[n // 2] * cache[n - n // 2]
+        return cache[exponent]
+
+    def eval(self, node: Parameter, derivate: int = 0):
+        """
+        Evaluates the hypernomio at given node
+
+        Example
+        -------
+        >>> hyper = Hypernomial([1, 2])
+        >>> print(hyper)
+        1 + 2*sinh(x)
+        >>> hyper.eval(0)
+        1
+        >>> hyper.eval(0, 0)
+        1
+        >>> hyper.eval(1, 0)
+        3.3504023872876029
+        """
+        if derivate != 0:
+            return self.derivate(derivate).eval(node, 0)
+        results = self[0]
+        for pi in range(self.pmax):
+            sinh = math.sinh(self.frequency * node)
+            cosh = math.cosh(self.frequency * node)
+            results += self[2 * pi + 1] * sinh + self[2 * pi + 2] * cosh
+        return results
+
+    def derivate(self, times: int = 1) -> Hypernomial:
+        times = int(times)
+        if times < 0:
+            raise ValueError
+        if times == 0:
+            return self.__class__(tuple(self), self.frequency)
+        const = self.frequency**times
+        coefs = list(self)
+        coefs[0] *= 0
+        for i in range(self.pmax):
+            term = (i + 1) ** times
+            coefs[2 * i + 1] *= const * term
+            coefs[2 * i + 2] *= const * term
+        times %= 4
+        if times % 2:
+            for i in range(self.pmax):
+                sinh = coefs[2 * i + 1]
+                cosh = coefs[2 * i + 2]
+                coefs[2 * i + 1] = cosh
+                coefs[2 * i + 2] = sinh
+        return self.__class__(coefs, self.frequency)
+
+    def integrate(self, times: int) -> Hypernomial:
+        times = int(times)
+        if times < 0:
+            raise ValueError
+        if times == 0:
+            return self.__class__(tuple(self), self.frequency)
+        if self[0]:
+            raise ValueError("Cannot integrate the function with constant")
+        const = self.frequency**times
+        coefs = list(self)
+        for i in range(self.pmax):
+            term = (i + 1) ** times
+            coefs[2 * i + 1] /= const * term
+            coefs[2 * i + 2] /= const * term
+        times %= 4
+        if times % 2:
+            for i in range(self.pmax):
+                sinh, cosh = coefs[2 * i + 1], coefs[2 * i + 2]
+                coefs[2 * i + 1] = cosh
+                coefs[2 * i + 2] = sinh
+        return self.__class__(coefs, self.frequency)
+
+    def defintegral(self, lower: Parameter, upper: Parameter) -> Scalar:
+        const = self[0]
+        self[0] *= 0
+        inthyper = self.integrate(1)
+        value = inthyper.eval(upper, 0) - inthyper.eval(lower, 0)
+        value += const * (upper - lower)
+        self[0] += const
+        return value
+
+    def shift(self, amount: Parameter) -> Hypernomial:
+        """
+        Evaluates the hypernomio at given node
+
+        Example
+        -------
+        >>> old_hyper = Hypernomial([1, 2])
+        >>> print(old_hyper)
+        1 + 2*sinh(x)
+        >>> new_hyper = old_hyper.shift(1/4)
+        >>> print(new_hyper)
+        1 - 2*cosh(x)
+        """
+        raise NotImplementedError
+
+    def scale(self, value: Scalar) -> Hypernomial:
+        """
+        Returns a function g(t) = f(a*t)
+        Where a is the given parameter
+        """
+        return self.__class__(tuple(self), value * self.frequency)
+
+    def roots(
+        self, inflim: Optional[Parameter], suplim: Optional[Parameter]
+    ) -> Iterable[Parameter]:
+        raise NotImplementedError
+
+    def __call__(self, nodes):
+        return self.eval(nodes, 0)
+
+    def __str__(self) -> str:
+        if self.pmax == 0:
+            return str(self[0])
+        msgs = []
+        flag = False
+        for i, coef in enumerate(self):
+            if coef == 0:
+                continue
+            if coef < 0:
+                msg = "- "
+            elif flag:
+                msg = "+ "
+            else:
+                msg = ""
+            flag = True
+            p = (i + 1) // 2
+            coef = abs(coef)
+            if coef != 1 or i == 0:
+                msg += str(coef)
+            if i > 0:
+                if coef != 1:
+                    msg += " * "
+                msg += "sinh(" if i % 2 else "cosh("
+                if p * self.frequency != 1:
+                    msg += f"{p*self.frequency}*"
+                msg += "t)"
+            msgs.append(msg)
+        return " ".join(msgs)
+
+    def __repr__(self) -> str:
+        return str(self)
---
+++ tests/analytic/test_hyperbolic.py
@@ -0,0 +1,305 @@
+"""
+This file contains tests functions to test the module polygon.py
+"""
+
+import math
+
+import numpy as np
+import pytest
+import sympy as sp
+
+from shapepy.analytic.hyperbolic import Hypernomial
+
+
+@pytest.mark.order(3)
+@pytest.mark.dependency()
+def test_begin():
+    pass
+
+
+@pytest.mark.order(3)
+@pytest.mark.dependency(depends=["test_begin"])
+def test_build():
+    Hypernomial([1])  # p(x) = 1
+    Hypernomial([1, 1])  # p(x) = 1 + sin(2*pi*x)
+    Hypernomial([1, 2])  # p(x) = 1 + 2*sin(2*pi*x)
+    Hypernomial([1, 3, 2])  # 1 + 3*sin(2*pi*x) + 2*cos(2*pi*x)
+    Hypernomial([1.0, 2.0, 3.0])
+
+
+@pytest.mark.order(3)
+@pytest.mark.dependency(depends=["test_begin"])
+def test_degree():
+    assert Hypernomial([1]).pmax == 0
+    assert Hypernomial([1, 1]).pmax == 1
+    assert Hypernomial([1, 2]).pmax == 1
+    assert Hypernomial([1, 3, 2]).pmax == 1
+    assert Hypernomial([1.0, 2.0, 3.0]).pmax == 1
+    assert Hypernomial([1, 3, 2, 4, 5]).pmax == 2
+
+
+@pytest.mark.order(3)
+@pytest.mark.dependency(depends=["test_degree"])
+def test_simplify():
+    coefs = [1, 2, 3, 0]
+    polya = Hypernomial(coefs)
+    assert polya.pmax == 1
+    assert tuple(polya) == (1, 2, 3)
+
+
+@pytest.mark.order(3)
+@pytest.mark.dependency(depends=["test_build"])
+def test_evaluate_natural():
+    trig = Hypernomial([1])  # p(x) = 1
+    assert trig.eval(0) == 1
+    assert trig.eval(1) == 1
+    assert trig.eval(2) == 1
+    assert trig.eval(1.0) == 1
+
+    trig = Hypernomial([1, 2])  # p(x) = 1 + 2*sinh(x)
+    assert trig.eval(0) == 1
+    assert trig.eval(1) == 1 + 2 * math.sinh(1)
+    assert trig.eval(2) == 1 + 2 * math.sinh(2)
+    assert trig.eval(0.0) == 1
+    assert trig.eval(1.0) == 1 + 2 * math.sinh(1)
+
+    trig = Hypernomial([1, 2, 3])  # p(x) = 1 + 2*sinh(x) + 3*cos(x)
+    assert trig.eval(0) == 4
+    assert trig.eval(1) == 1 + 2 * math.sinh(1) + 3 * math.cosh(1)
+    assert trig.eval(2) == 1 + 2 * math.sinh(2) + 3 * math.cosh(2)
+    assert trig.eval(0.0) == 4
+    assert trig.eval(1.0) == 1 + 2 * math.sinh(1) + 3 * math.cosh(1)
+
+    assert trig(1.0) == 1 + 2 * math.sinh(1) + 3 * math.cosh(1)
+
+
+@pytest.mark.order(3)
+@pytest.mark.dependency(depends=["test_degree"])
+def test_compare():
+    coefs = [3, 5, -4, 3, 2]
+    polya = Hypernomial(coefs)
+    coefs = [3, 5, -4, 3, 2, 7]
+    polyb = Hypernomial(coefs)
+    assert polya != polyb
+
+    coefs = [3, 5, -4, 3, 2]
+    polya = Hypernomial(coefs)
+    coefs = [3, 5, -4, 3, 2]
+    polyb = Hypernomial(coefs)
+    assert polya == polyb
+
+    coefs = [3, 5, -4, 3, 2]
+    polya = Hypernomial(coefs)
+    coefs = [3, -3, -4, 3, 2]
+    polyb = Hypernomial(coefs)
+    assert polya != polyb
+
+    trig = Hypernomial([0])
+    assert trig == 0
+    trig = Hypernomial([1])
+    assert trig == 1
+    trig = Hypernomial([2])
+    assert trig == 2
+
+
+@pytest.mark.order(3)
+@pytest.mark.dependency(depends=["test_compare"])
+def test_add():
+    polya = Hypernomial([1, 3])
+    assert polya + 3 == Hypernomial([4, 3])
+    assert 3 + polya == Hypernomial([4, 3])
+
+    polya = Hypernomial([1, 2, 3])
+    polyb = Hypernomial([3, 2, 1])
+    assert polya + polyb == Hypernomial([4, 4, 4])
+
+    polya = Hypernomial([1, 2, 3])
+    polyb = Hypernomial([3, 2, 1, 7])
+    assert polya + polyb == Hypernomial([4, 4, 4, 7])
+
+    polya = Hypernomial([1, 2, 3, 7])
+    polyb = Hypernomial([3, 2, 1])
+    assert polya + polyb == Hypernomial([4, 4, 4, 7])
+
+
+@pytest.mark.order(3)
+@pytest.mark.dependency(depends=["test_compare", "test_add"])
+def test_sub():
+    polya = Hypernomial([1, 3])
+    assert polya - 3 == Hypernomial([-2, 3])
+    assert 3 - polya == Hypernomial([2, -3])
+
+
+@pytest.mark.order(3)
+@pytest.mark.timeout(3)
+@pytest.mark.dependency(depends=["test_compare", "test_add"])
+def test_mult():
+    sinht = Hypernomial([0, 1, 0])
+    cosht = Hypernomial([0, 0, 1])
+    sinh2t = Hypernomial([0, 0, 0, 1, 0])
+    cosh2t = Hypernomial([0, 0, 0, 0, 1])
+
+    val = cosht * cosht - sinht * sinht
+    print("1 = ", val)
+    assert cosht * cosht - sinht * sinht == 1
+    assert cosh2t * cosh2t - sinh2t * sinh2t == 1
+    assert 2 * sinht * cosht == sinh2t
+    assert cosht * cosht + sinht * sinht == cosh2t
+    assert cosh2t + 1 == 2 * cosht * cosht
+    assert cosh2t - 1 == 2 * sinht * sinht
+
+
+@pytest.mark.order(3)
+@pytest.mark.timeout(3)
+@pytest.mark.dependency(depends=["test_compare", "test_mult"])
+def test_pow():
+    sinht = Hypernomial([0, 1, 0])
+    cosht = Hypernomial([0, 0, 1])
+    sinh2t = Hypernomial([0, 0, 0, 1, 0])
+    cosh2t = Hypernomial([0, 0, 0, 0, 1])
+
+    assert sinht**0 == 1
+    assert cosht**0 == 1
+    assert sinh2t**0 == 1
+    assert cosh2t**0 == 1
+    assert sinht**1 == sinht
+    assert cosht**1 == cosht
+    assert sinh2t**1 == sinh2t
+    assert cosh2t**1 == cosh2t
+
+    assert cosht**2 - sinht**2 == 1
+    assert cosh2t**2 - sinh2t**2 == 1
+    assert 2 * sinht * cosht == sinh2t
+    assert cosht**2 + sinht**2 == cosh2t
+    assert cosh2t + 1 == 2 * cosht**2
+    assert cosh2t - 1 == 2 * sinht**2
+
+
+@pytest.mark.order(3)
+@pytest.mark.dependency(depends=["test_compare"])
+def test_derivate():
+    coefs = [3, 5, -4, 3, 2]
+    trig = Hypernomial(coefs)
+    test = trig.derivate(0)
+    assert test == trig
+
+    test = trig.derivate(1)
+    good = Hypernomial([0, -4, 5, 4, 6])
+    assert test != trig
+    assert test == good
+
+
+@pytest.mark.order(3)
+@pytest.mark.dependency(depends=["test_derivate"])
+def test_integrate():
+    coefs = [0, 5, -4, 3, 2]
+    trig = Hypernomial(coefs)
+    test = trig.integrate(0)
+    assert test == trig
+
+    for times in range(5):
+        test = trig.derivate(times)
+        test = test.integrate(times)
+        assert test == trig
+
+
+@pytest.mark.order(3)
+@pytest.mark.dependency(depends=["test_evaluate_natural", "test_derivate"])
+def test_evaluate_derivate():
+    trig = Hypernomial([1])  # p(x) = 1
+    assert trig.eval(0, 1) == 0
+    assert trig.eval(1, 1) == 0
+    assert trig.eval(2, 1) == 0
+    assert trig.eval(1.0, 1) == 0
+    assert trig.eval(1, 2) == 0
+
+    trig = Hypernomial([1, 2])  # p(x) = 1 + 2*sinh(x)
+    assert trig.eval(0, 1) == 2
+    assert trig.eval(1, 1) == 2 * math.cosh(1)
+    assert trig.eval(2, 1) == 2 * math.cosh(2)
+    assert trig.eval(0.0, 1) == 2
+    assert trig.eval(1.0, 1) == 2 * math.cosh(1)
+    assert trig.eval(2, 2) == 2 * math.sinh(2)
+    assert trig.eval(0.0, 2) == 0
+
+    trig = Hypernomial([1, 2, 3])  # p(x) = 1 + 2*sinh(x) + 3*cosh(x)
+    assert trig.eval(0, 1) == 2
+    assert trig.eval(1, 1) == 2 * math.cosh(1) + 3 * math.sinh(1)
+    assert trig.eval(2, 1) == 2 * math.cosh(2) + 3 * math.sinh(2)
+    assert trig.eval(0.0, 1) == 2
+    assert trig.eval(1.0, 1) == 2 * math.cosh(1) + 3 * math.sinh(1)
+    assert trig.eval(2, 2) == 2 * math.sinh(2) + 3 * math.cosh(2)
+    assert trig.eval(0.0, 2) == 3
+    assert trig.eval(2, 3) == 2 * math.cosh(2) + 3 * math.sinh(2)
+    assert trig.eval(0.0, 3) == 2
+
+
+@pytest.mark.order(3)
+@pytest.mark.dependency(depends=["test_build"])
+def test_print():
+    trig = Hypernomial([0])
+    assert str(trig) == "0"
+    trig = Hypernomial([0, 0])
+    assert str(trig) == "0"
+    trig = Hypernomial([0, 0, 0])
+    assert str(trig) == "0"
+    trig = Hypernomial([1])
+    assert str(trig) == "1"
+    trig = Hypernomial([1, 1])
+    assert str(trig) == "1 + sinh(t)"
+    trig = Hypernomial([1, 1, 1])
+    assert str(trig) == "1 + sinh(t) + cosh(t)"
+    trig = Hypernomial([1, 0, 1])
+    assert str(trig) == "1 + cosh(t)"
+    trig = Hypernomial([0, 0, 1])
+    assert str(trig) == "cosh(t)"
+    trig = Hypernomial([-1])
+    assert str(trig) == "-1"
+    trig = Hypernomial([-1, 1])
+    assert str(trig) == "- 1 + sinh(t)"
+    trig = Hypernomial([1, -1, 1])
+    assert str(trig) == "1 - sinh(t) + cosh(t)"
+    trig = Hypernomial([1, 0, 1])
+    assert str(trig) == "1 + cosh(t)"
+    trig = Hypernomial([1, 2, 3])
+    assert str(trig) == "1 + 2 * sinh(t) + 3 * cosh(t)"
+    repr(trig)
+
+    trig = Hypernomial([1, 2, 3, 4, 5, 6, 7.9, 9])
+    str(trig)
+    repr(trig)
+
+
+@pytest.mark.order(3)
+@pytest.mark.dependency(depends=["test_integrate"])
+def test_definite_integrate():
+    # Integrate constant
+    trig = Hypernomial([0])
+    assert trig.defintegral(0, 1) == 0
+    for _ in range(100):  # ntests
+        const, lower, upper = np.random.randint(-10, 10, 3)
+        trig = Hypernomial([const])
+        assert trig.defintegral(lower, upper) == (upper - lower) * const
+
+
+@pytest.mark.order(3)
+@pytest.mark.dependency(
+    depends=[
+        "test_begin",
+        "test_build",
+        "test_degree",
+        "test_simplify",
+        "test_evaluate_natural",
+        "test_compare",
+        "test_derivate",
+        "test_integrate",
+        "test_evaluate_derivate",
+        "test_add",
+        "test_sub",
+        "test_mult",
+        "test_pow",
+        "test_definite_integrate",
+    ]
+)
+def test_end():
+    pass
