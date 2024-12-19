e3196675ca07e6c3790fac7549bff7431a021236
carlos-adir <carlos.adir.leite@gmail.com>
2024-12-19 09:55:07 -0300

    docs: add docstrings on shape __init__.py file

--- src/shapepy/shape/__init__.py
+++ src/shapepy/shape/__init__.py
@@ -1,1 +1,10 @@
+"""
+Module that contains the useful shapes:
+* SimpleShape
+* ConnectedShape
+* DisjointShape
+It's useful to import from the main package:
+
+from shapepy.shape import SimpleShape
+"""
 from .simple import ConnectedShape, DisjointShape, SimpleShape
