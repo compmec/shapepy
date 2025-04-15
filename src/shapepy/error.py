ddf6c5a13aaf4a39de3d6cc3550f06d422dfde19
Carlos Adir <carlos.adir.leite@gmail.com>
2025-04-15 20:58:59 +0200

    feat: add NotExpectedError class

---
+++ src/shapepy/error.py
@@ -0,0 +1,7 @@
+"""
+Definitions of common exceptions for :mod:`sympy.core` module.
+"""
+
+
+class NotExpectedError(Exception):
+    """Raised when arrives in a code section that were not expected"""
