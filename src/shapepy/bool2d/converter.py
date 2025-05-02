ed393a46b893e9851cefad418628ca9afbbf367f
Carlos Adir <carlos.adir.leite@gmail.com>
2025-05-02 21:37:20 +0200

    feat: implement functions to convert to SubSetR2

--- src/shapepy/bool2d/converter.py
+++ src/shapepy/bool2d/converter.py
@@ -6,9 +6,9 @@ The easier example is from string:
 * "{(-1, 1)}" represents a point, returns SinglePointR2 instance
 """
 
-from typing import Any
+from typing import Any, Dict, Set
 
-from .base import SubSetR2
+from .base import EmptyR2, SubSetR2
 
 
 def from_any(obj: Any) -> SubSetR2:
@@ -23,4 +23,39 @@ def from_any(obj: Any) -> SubSetR2:
     >>> ConverterR1.from_any("(-inf, +inf)")
     (-inf, +inf)
     """
-    return obj
+    if isinstance(obj, SubSetR2):
+        return obj
+    if isinstance(obj, str):
+        return from_str(obj)
+    if isinstance(obj, set):
+        return from_set(obj)
+    if isinstance(obj, dict):
+        return from_dict(obj)
+    raise NotImplementedError(f"Unsupported type {type(obj)}")
+
+
+def from_str(obj: str) -> SubSetR2:
+    """
+    Converts a string to a SubSetR2 instance
+    """
+    if obj == r"{}":
+        return EmptyR2()
+    raise NotImplementedError(str(obj))
+
+
+def from_set(obj: Set) -> SubSetR2:
+    """
+    Converts a string to a SubSetR2 instance
+    """
+    if len(obj) == 0:
+        return EmptyR2()
+    raise NotImplementedError(str(obj))
+
+
+def from_dict(obj: Dict) -> SubSetR2:
+    """
+    Converts a string to a SubSetR2 instance
+    """
+    if len(obj.keys()) == 0:
+        return EmptyR2()
+    raise NotImplementedError(str(obj))
