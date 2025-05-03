3a241891e4b58f1e92abf9204559da675d74fbca
Carlos Adir <carlos.adir.leite@gmail.com>
2025-05-03 13:50:27 +0200

    test: add tests for transformation of single point

--- tests/bool2d/test_transform.py
+++ tests/bool2d/test_transform.py
@@ -2,6 +2,7 @@ import pytest
 
 from shapepy.angle import Angle
 from shapepy.bool2d.base import EmptyR2, WholeR2
+from shapepy.bool2d.singles import SinglePointR2
 from shapepy.bool2d.transform import move, rotate, scale
 
 
@@ -95,10 +96,73 @@ class TestWhole:
             assert rotate(whole, angle) == whole
 
 
+class TestSinglePointR2:
+
+    @pytest.mark.order(20)
+    @pytest.mark.timeout(1)
+    @pytest.mark.dependency(
+        depends=[
+            "tests/bool2d/test_point.py::test_all",
+        ],
+        scope="session",
+    )
+    def test_begin(self):
+        pass
+
+    @pytest.mark.order(20)
+    @pytest.mark.timeout(1)
+    @pytest.mark.dependency(depends=["TestSinglePointR2::test_begin"])
+    def test_move(self):
+        points = [(-10, -10), (5, -3), (-3, 7)]
+        vectors = [(30, 2), (4, -2), (0, 0)]
+        for point in points:
+            single = SinglePointR2(point)
+            for vector in vectors:
+                good = (point[0] + vector[0], point[1] + vector[1])
+                good = SinglePointR2(good)
+                assert single.move(vector) == good
+                assert move(single, vector) == good
+
+    @pytest.mark.order(20)
+    @pytest.mark.timeout(1)
+    @pytest.mark.dependency(depends=["TestSinglePointR2::test_begin"])
+    def test_scale(self):
+        single = SinglePointR2((3, 4))
+        assert single.scale(1) == single
+        assert scale(single, 1) == single
+
+        assert single.scale((1, 1)) == single
+        assert scale(single, (1, 1)) == single
+
+        assert single.scale(3) == SinglePointR2((9, 12))
+        assert scale(single, 3) == SinglePointR2((9, 12))
+
+        assert single.scale((3, 4)) == SinglePointR2((9, 16))
+        assert scale(single, (3, 4)) == SinglePointR2((9, 16))
+
+    @pytest.mark.order(20)
+    @pytest.mark.timeout(1)
+    @pytest.mark.dependency(depends=["TestSinglePointR2::test_begin"])
+    def test_rotate(self):
+        point = SinglePointR2((3, 4))
+
+        angle = Angle.degrees(0)
+        assert point.rotate(angle) == point
+        assert rotate(point, angle) == point
+
+        angle = Angle.degrees(90)
+        assert point.rotate(angle) == SinglePointR2((-4, 3))
+        assert rotate(point, angle) == SinglePointR2((-4, 3))
+
+
 @pytest.mark.order(20)
 @pytest.mark.timeout(1)
 @pytest.mark.dependency(
-    depends=["TestEmpty::test_move", "TestWhole::test_move"]
+    depends=[
+        "TestEmpty::test_move",
+        "TestWhole::test_move",
+        "TestSinglePointR2::test_move",
+    ]
 )
 def test_move():
     pass
@@ -107,7 +171,11 @@ def test_move():
 @pytest.mark.order(20)
 @pytest.mark.timeout(1)
 @pytest.mark.dependency(
-    depends=["TestEmpty::test_scale", "TestWhole::test_scale"]
+    depends=[
+        "TestEmpty::test_scale",
+        "TestWhole::test_scale",
+        "TestSinglePointR2::test_scale",
+    ]
 )
 def test_scale():
     pass
@@ -116,7 +184,11 @@ def test_scale():
 @pytest.mark.order(20)
 @pytest.mark.timeout(1)
 @pytest.mark.dependency(
-    depends=["TestEmpty::test_rotate", "TestWhole::test_rotate"]
+    depends=[
+        "TestEmpty::test_rotate",
+        "TestWhole::test_rotate",
+        "TestSinglePointR2::test_rotate",
+    ]
 )
 def test_rotate():
     pass
