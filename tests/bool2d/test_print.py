8c0c185907c19ffd8935ff4ec6d73d42a417353f
Carlos Adir <carlos.adir.leite@gmail.com>
2025-05-03 14:24:29 +0200

    test: add test for printing

---
+++ tests/bool2d/test_print.py
@@ -0,0 +1,183 @@
+import pytest
+
+from shapepy.bool2d.base import EmptyR2, WholeR2
+from shapepy.bool2d.bool2d import intersect, invert, unite
+from shapepy.bool2d.singles import SinglePointR2
+
+
+class TestStr:
+
+    @pytest.mark.order(50)
+    @pytest.mark.timeout(1)
+    @pytest.mark.dependency(
+        depends=[
+            "tests/bool2d/test_empty_whole.py::test_all",
+        ],
+        scope="session",
+    )
+    def test_empty(self):
+        empty = EmptyR2()
+        assert str(empty) == "EmptyR2"
+
+    @pytest.mark.order(50)
+    @pytest.mark.timeout(1)
+    @pytest.mark.dependency(
+        depends=[
+            "tests/bool2d/test_empty_whole.py::test_all",
+        ],
+        scope="session",
+    )
+    def test_whole(self):
+        whole = WholeR2()
+        assert str(whole) == "WholeR2"
+
+    @pytest.mark.order(50)
+    @pytest.mark.timeout(1)
+    @pytest.mark.dependency(
+        depends=[
+            "tests/bool2d/test_point.py::test_all",
+        ],
+        scope="session",
+    )
+    def test_point(self):
+        point = SinglePointR2((-1, 1))
+        assert str(point) == "{(-1, 1)}"
+
+    @pytest.mark.order(50)
+    @pytest.mark.timeout(1)
+    @pytest.mark.dependency(
+        depends=[
+            "tests/bool2d/test_point.py::test_all",
+        ],
+        scope="session",
+    )
+    def test_not_point(self):
+        point = ~SinglePointR2((-1, 1))
+        assert str(point) == "NOT[{(-1, 1)}]"
+
+    @pytest.mark.order(50)
+    @pytest.mark.timeout(1)
+    @pytest.mark.dependency(
+        depends=[
+            "tests/bool2d/test_point.py::test_all",
+        ],
+        scope="session",
+    )
+    def test_or_point(self):
+        points = [(-1, -1), (1, 1)]
+        subset = unite(*map(SinglePointR2, points))
+        assert str(subset) == "OR[{(1, 1)}, {(-1, -1)}]"
+
+    @pytest.mark.order(50)
+    @pytest.mark.timeout(1)
+    @pytest.mark.dependency(
+        depends=[
+            "tests/bool2d/test_point.py::test_all",
+        ],
+        scope="session",
+    )
+    def test_and_not_point(self):
+        points = [(-1, -1), (1, 1)]
+        subset = intersect(*map(invert, map(SinglePointR2, points)))
+        assert str(subset) == "AND[NOT[{(-1, -1)}], NOT[{(1, 1)}]]"
+
+
+class TestRepr:
+
+    @pytest.mark.order(50)
+    @pytest.mark.timeout(1)
+    @pytest.mark.dependency(
+        depends=[
+            "tests/bool2d/test_empty_whole.py::test_all",
+        ],
+        scope="session",
+    )
+    def test_empty(self):
+        empty = EmptyR2()
+        assert repr(empty) == "EmptyR2"
+
+    @pytest.mark.order(50)
+    @pytest.mark.timeout(1)
+    @pytest.mark.dependency(
+        depends=[
+            "tests/bool2d/test_empty_whole.py::test_all",
+        ],
+        scope="session",
+    )
+    def test_whole(self):
+        whole = WholeR2()
+        assert repr(whole) == "WholeR2"
+
+    @pytest.mark.order(50)
+    @pytest.mark.timeout(1)
+    @pytest.mark.dependency(
+        depends=[
+            "tests/bool2d/test_point.py::test_all",
+        ],
+        scope="session",
+    )
+    def test_point(self):
+        point = SinglePointR2((-1, 1))
+        assert repr(point) == "SinglePointR2((-1, 1))"
+
+    @pytest.mark.order(50)
+    @pytest.mark.timeout(1)
+    @pytest.mark.dependency(
+        depends=[
+            "tests/bool2d/test_point.py::test_all",
+        ],
+        scope="session",
+    )
+    def test_not_point(self):
+        point = ~SinglePointR2((-1, 1))
+        assert repr(point) == "NOT[SinglePointR2((-1, 1))]"
+
+    @pytest.mark.order(50)
+    @pytest.mark.timeout(1)
+    @pytest.mark.dependency(
+        depends=[
+            "tests/bool2d/test_point.py::test_all",
+        ],
+        scope="session",
+    )
+    def test_or_point(self):
+        points = [(-1, -1), (1, 1)]
+        subset = unite(*map(SinglePointR2, points))
+        good = "OR[SinglePointR2((1, 1)), SinglePointR2((-1, -1))]"
+        assert repr(subset) == good
+
+    @pytest.mark.order(50)
+    @pytest.mark.timeout(1)
+    @pytest.mark.dependency(
+        depends=[
+            "tests/bool2d/test_point.py::test_all",
+        ],
+        scope="session",
+    )
+    def test_and_not_point(self):
+        points = [(-1, -1), (1, 1)]
+        subset = intersect(*map(invert, map(SinglePointR2, points)))
+        good = "AND[NOT[SinglePointR2((-1, -1))], NOT[SinglePointR2((1, 1))]]"
+        assert repr(subset) == good
+
+
+@pytest.mark.order(50)
+@pytest.mark.timeout(1)
+@pytest.mark.dependency(
+    depends=[
+        "TestStr::test_empty",
+        "TestStr::test_whole",
+        "TestStr::test_point",
+        "TestStr::test_not_point",
+        "TestStr::test_or_point",
+        "TestStr::test_and_not_point",
+        "TestRepr::test_empty",
+        "TestRepr::test_whole",
+        "TestRepr::test_point",
+        "TestRepr::test_not_point",
+        "TestRepr::test_or_point",
+        "TestRepr::test_and_not_point",
+    ]
+)
+def test_all():
+    pass
