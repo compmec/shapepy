951af847c6672b24e9feae5e2153ea78d9c7a8e4
Carlos Adir <carlos.adir.leite@gmail.com>
2025-05-03 18:43:50 +0200

    test: add more conversion tests to bool2d

--- tests/bool2d/test_convert.py
+++ tests/bool2d/test_convert.py
@@ -1,7 +1,9 @@
 import pytest
 
 from shapepy.bool2d.base import EmptyR2
+from shapepy.bool2d.bool2d import intersect, invert, unite
 from shapepy.bool2d.converter import from_any
+from shapepy.bool2d.singles import PointR2
 
 
 @pytest.mark.order(55)
@@ -9,6 +11,19 @@ from shapepy.bool2d.converter import from_any
 @pytest.mark.dependency()
 def test_from_string():
     assert from_any(r"{}") == EmptyR2()
+    assert from_any(r"{(-10, 10)}") == PointR2((-10, 10))
+
+    text = "NOT[{(-10, 10)}]"
+    subset = invert(PointR2((-10, 10)))
+    assert from_any(text) == subset
+
+    text = "OR[{(-10, 10)}, {(10, 10)}]"
+    union = unite(PointR2((-10, 10)), PointR2((10, 10)))
+    assert from_any(text) == union
+
+    text = "AND[{(-10, 10)}, {(10, 10)}]"
+    subset = intersect(PointR2((-10, 10)), PointR2((10, 10)))
+    assert from_any(text) == subset
 
 
 @pytest.mark.order(55)
@@ -16,6 +31,11 @@ def test_from_string():
 @pytest.mark.dependency()
 def test_from_set():
     assert from_any(set()) == EmptyR2()
+
+    obj = {(-10, 10), (10, 10)}
+    points = [(-10, 10), (10, 10)]
+    union = unite(*map(PointR2, points))
+    assert from_any(obj) == union
 
 
 @pytest.mark.order(55)
