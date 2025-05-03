3e10a84fb0ac0f5a645aa25f5277294b85e05172
Carlos Adir <carlos.adir.leite@gmail.com>
2025-05-03 15:58:03 +0200

    feat: add primitive functions that creates some basic shapes

---
+++ src/shapepy/primitive.py
@@ -0,0 +1,159 @@
+"""
+File that contains functios to create classical shapes such as
+`triangle`, `square`, `regular_polygon` and a generic `polygon`
+
+"""
+
+from numbers import Real
+from typing import Iterable
+
+from shapepy.bool2d.singles import ShapeR2
+
+from . import default
+from .analytic.elementar import linear_piecewise
+from .angle import Angle
+from .geometry import GeometricPoint, JordanCurve, geometric_point
+
+
+def polygon(vertices: Iterable[GeometricPoint]) -> ShapeR2:
+    """
+    Creates a generic polygon
+
+    Parameters
+    ----------
+    vertices: tuple[GeometricPoint]
+        Vertices of the polygon
+
+    Return
+    ------
+    ShapeR2
+        The ShapeR2 instance that represents the polygon
+
+    Example
+    -------
+    >>> from shapepy import Primitive
+    >>> vertices = [(1, 0), (0, 1), (-1, 1), (0, -1)]
+    >>> shape = polygon(vertices)
+
+    .. image:: ../img/primitive/diamond.svg
+
+    """
+    vertices = tuple(map(geometric_point, vertices))
+    knots = tuple(map(default.finite, range(len(vertices) + 1)))
+    xvalues = list(vertex.x for vertex in vertices)
+    yvalues = list(vertex.y for vertex in vertices)
+    xvalues.append(xvalues[0])
+    yvalues.append(yvalues[0])
+    xanalytic = linear_piecewise(xvalues, knots)
+    yanalytic = linear_piecewise(yvalues, knots)
+    jordan_curve = JordanCurve(xanalytic, yanalytic)
+    return ShapeR2(jordan_curve)
+
+
+def triangle(side: float = 1, center: GeometricPoint = (0, 0)) -> ShapeR2:
+    """
+    Create a right triangle
+
+    Parameters
+    ----------
+    side : Real, default: 1
+        Width and height of the triangle
+    center : GeometricPoint, default: (0, 0)
+        Position of the vertex of right angle
+
+    Return
+    ------
+    ShapeR2
+        The ShapeR2 instance that represents the triangle
+
+    Example use
+    -----------
+    >>> from shapepy import Primitive
+    >>> triangle = triangle()
+
+    .. image:: ../img/primitive/triangle.svg
+
+    """
+    side = default.finite(side)
+    center = geometric_point(center)
+    vertices = [(0, 0), (side, 0), (0, side)]
+    return polygon(vertices).move(center)
+
+
+def square(side: Real = 1, center: GeometricPoint = (0, 0)) -> ShapeR2:
+    """
+    Creates a square with sides aligned with axis
+
+    Parameters
+    ----------
+    side : Real, default: 1
+        Side of the square.
+    center : GeometricPoint, default: (0, 0)
+        The geometric center of the square
+
+    Return
+    ------
+    ShapeR2
+        The simple shape that represents the square
+
+    Example use
+    -----------
+    >>> from shapepy import Primitive
+    >>> square = square()
+
+    .. image:: ../img/primitive/square.svg
+
+    """
+    half_side = default.finite(side) / 2
+    center = geometric_point(center)
+    vertices = [
+        (half_side, half_side),
+        (-half_side, half_side),
+        (-half_side, -half_side),
+        (half_side, -half_side),
+    ]
+    return polygon(vertices).move(center)
+
+
+def regular_polygon(
+    nsides: int, radius: Real = 1, center: GeometricPoint = (0, 0)
+) -> ShapeR2:
+    """
+    Creates a regular polygon of `nsides` sides
+
+    Parameters
+    ----------
+    nsides : int
+        Number of sides of regular polygon, >= 3
+    radius : Real, default: 1
+        Radius of the external circle that contains the polygon.
+    center : GeometricPoint, default: (0, 0)
+        The geometric center of the regular polygon
+
+    Return
+    ------
+    ShapeR2
+        The simple shape that represents the regular polygon
+
+    Example use
+    -----------
+    >>> from shapepy import primitive
+    >>> triangle = primitive.regular_polygon(nsides = 3)
+
+    .. image:: ../img/primitive/regular3.svg
+
+    .. image:: ../img/primitive/regular4.svg
+
+    .. image:: ../img/primitive/regular5.svg
+
+    """
+    nsides = default.integer(nsides)
+    radius = default.finite(radius)
+    center = geometric_point(center)
+
+    vertices = []
+    for i in range(nsides):
+        angle = Angle.turns(default.rational(i, nsides))
+        vertex = GeometricPoint.polar(radius, angle)
+        vertices.append(vertex)
+    return polygon(vertices).move(center)
