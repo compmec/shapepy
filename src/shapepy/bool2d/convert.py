d8a4efef22f6fd44fe5ec67b83f4972642f107b8
Carlos Adir <carlos.adir.leite@gmail.com>
2025-09-20 18:59:47 +0200

    feat: add conversion from objects to Point or Curve

--- src/shapepy/bool2d/convert.py
+++ src/shapepy/bool2d/convert.py
@@ -2,14 +2,19 @@
 Functions to convert from typical data types into SubSetR2
 """
 
-from ..tools import Is
+from ..geometry.base import IGeometricCurve
+from ..tools import Is, To
 from .base import SubSetR2
+from .curve import SingleCurve
+from .point import SinglePoint
 
 
 def from_any(subset: SubSetR2) -> SubSetR2:
     """
     Converts an object into a SubSetR2
     """
-    if not Is.instance(subset, SubSetR2):
-        raise TypeError
-    return subset
+    if Is.instance(subset, SubSetR2):
+        return subset
+    if Is.instance(subset, IGeometricCurve):
+        return SingleCurve(subset)
+    return SinglePoint(To.point(subset))
